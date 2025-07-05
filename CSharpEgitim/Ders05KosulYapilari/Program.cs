b4ed8386d83097f969692c628fecbaa7017b7ace
Miraç Akkoyun <miracakkoyun00@gmail.com>
2025-07-05 22:21:08 +0300

    go

---
+++ CSharpEgitim/Ders05KosulYapilari/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konu05KosulYapilari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * String admin="admin";
+            String password="1234";
+
+            while (true)
+            {
+                Console.WriteLine("Kullanıcı Adınızı Giriniz:");
+                String kullaniciAdi = Console.ReadLine();
+                Console.WriteLine("Parolanızı Giriniz:");
+                String parola = Console.ReadLine();
+                if (kullaniciAdi == admin && parola == password)
+                {
+                    Console.WriteLine("Giriş Başarılı");
+                    break;
+                }
+                else if (kullaniciAdi != admin && parola == password)
+                {
+                    Console.WriteLine("Hatali Kullanici Adi Lutfen Tekrar Giris Yapin");
+                }
+                else if (kullaniciAdi == admin && parola != password)
+                {
+                    Console.WriteLine("Hatali Parola Lutfen Tekrar Giris Yapin");
+                }
+                else
+                {
+                    Console.WriteLine("Hatali Kullanici Adi ve Parola Lutfen Tekrar Giris Yapin");
+                }
+            }*/
+
+
+            //switch-case yapısı
+            //Console.WriteLine("1 ile 12 arasinda bir sayi girin : ");
+            int ay = DateTime.Now.Month;//Sistemden tarighi aldi 
+            //Convert.ToInt32(Console.ReadLine());
+            switch (ay)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kis Mevsimi");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Ilkbahar Mevsimi");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Mevsimi");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar Mevsimi");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ CSharpEgitim/Konu06Donguler/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konu06Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Console.WriteLine("For DOngusu");
+            int toplam = 0;
+            for(int i = 1; i <= 10; i++)
+            {
+                toplam += i;
+                Console.WriteLine("Toplam: "+toplam);
+            }*/
+
+
+
+            //while dongusu 
+            /*while (true)
+            {
+                Console.WriteLine("Cikis yapmak icin 'q' ya basin");
+                Console.WriteLine("Deger giriniz");
+                string deger = Console.ReadLine();
+                if(deger== "q")
+                {
+                    Console.WriteLine("Cikis yapiliyor...");
+                    break; // donguden cik
+                }
+                else
+                {
+                    try
+                    {
+                        int sayi = Convert.ToInt32(deger);
+                        Console.WriteLine("Girdiginiz deger: " + sayi);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Hata: " + ex.Message);
+                    }
+                }
+            }*/
+
+
+
+            /*Console.WriteLine("Do while dongusu");
+            int toplam = 3;
+            do
+            {
+                Console.WriteLine("Do while sayi {0}",toplam);
+                toplam += 1;
+            } while (toplam < 10);
+        
+            */
+
+
+            //FOr each dongusu
+            /*int[] sayilar = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("For each dongusu");
+            foreach (int sayi in sayilar) {
+                Console.WriteLine("Sayi: "+sayi);
+            }*/
+
+
+        }
+    }
+}
